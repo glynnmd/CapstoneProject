3d4bdbfe21bc2864f1cacde87c98960cc6329a80
Glynn <glynnmd@appd.appstate.edu>
2018-02-07 18:16:54 -0500

    End Class 2/7/18

--- app/src/main/java/com/example/glynnmd/helloworld/HelloActivity.java
+++ app/src/main/java/com/example/glynnmd/helloworld/HelloActivity.java
@@ -1,5 +1,6 @@
 package com.example.glynnmd.helloworld;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,14 +23,18 @@ public class HelloActivity extends Activity {
 
         Button sub = (Button) findViewById(R.id.sub);
         sub.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
                 EditText carbs = (EditText) findViewById(R.id.carbs);
                 EditText fats = (EditText) findViewById(R.id.fats);
                 EditText proteins = (EditText) findViewById(R.id.proteins);
                 TextView currentmacros = (TextView) findViewById(R.id.currentmacros);
-                currentmacros.setText("Carbs :" + carbs.getText().toString() + "\n Fats : " +
-                fats.getText().toString() + "\n Proteins : " + proteins.getText().toString());
+                TextView calories = (TextView) findViewById(R.id.calories);
+                currentmacros.setText("Carbs : " + Integer.parseInt(carbs.getText().toString()) + "  Fats : "
+                        + Integer.parseInt(fats.getText().toString()) + "  Proteins : " + Integer.parseInt(proteins.getText().toString()));
+                int cal = (Integer.parseInt(proteins.getText().toString()) * 4) + (Integer.parseInt(fats.getText().toString()) * 9) + (Integer.parseInt(carbs.getText().toString()) * 4)
+;                calories.setText("Calories :" + cal);
                 carbvalue = Integer.parseInt(carbs.getText().toString());
                 fatsvalue = Integer.parseInt(fats.getText().toString());
                 proteinsvalue = Integer.parseInt(proteins.getText().toString());
@@ -38,20 +43,24 @@ public class HelloActivity extends Activity {
 
         Button sub2 = (Button) findViewById(R.id.sub2);
         sub2.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
                 EditText mealcarbs = (EditText) findViewById(R.id.mealcarbs);
                 EditText mealfats = (EditText) findViewById(R.id.mealfats);
                 EditText mealproteins = (EditText) findViewById(R.id.mealproteins);
                 TextView macrosleft = (TextView) findViewById(R.id.macrosleft);
-                if(carbvalue != null && fatsvalue != null && proteinsvalue != null) {
+
                     int carbl = carbvalue - Integer.parseInt(mealcarbs.getText().toString());
                     int fatsl = fatsvalue - Integer.parseInt(mealfats.getText().toString());
                     int proteinsl = proteinsvalue - Integer.parseInt(mealproteins.getText().toString());
+                    carbvalue = carbl;
+                    fatsvalue = fatsl;
+                    proteinsvalue = proteinsl;
 
-                    macrosleft.setText("Carbs left:" + carbl + "\n Fats : " +
-                            fatsl + "\n Proteins : " + proteinsl);
-                }
+                    macrosleft.setText("Carbs : " + carbl + " Fats : " +
+                            fatsl + " Proteins : " + proteinsl);
+
             }
         });
     }
